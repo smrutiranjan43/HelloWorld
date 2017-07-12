a6c876eadb5b5d654150be91c328bb0e29b1ce31
Smruti Ranjan <smrutiranjan.patra@votarytech.com>
2017-07-12 12:19:00 +0530

    Three files added in br1

--- file1.c
+++ file1.c
@@ -3,4 +3,7 @@ main()
 {
 int i=3,j=7;
 printf("%d\n",i*j);
+printf("%d\n",i*j);
+printf("%d\n",i*j);
+
 }
--- main.c
+++ main.c
@@ -3,6 +3,8 @@ int main()
 {
 int i=3,j=4;
 printf("sum=%d\n",i+j);
+printf("sum=%d\n",i+j);
+printf("sum=%d\n",i+j);
 return 0;
 }
 
